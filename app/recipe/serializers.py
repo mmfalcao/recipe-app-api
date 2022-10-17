6916a463eb536ca3dd954468a212379ad81a74ff
mmfalcao <marcelmf@msn.com>
2022-10-16 23:32:32 -0300

    fix lint recipe module

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 
 from core.models import Recipe
 
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipe"""
 
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -196,4 +196,3 @@ class PrivateRecipeApiTests(TestCase):
         res = self.client.delete(url)
         self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
         self.assertTrue(Recipe.objects.filter(id=recipe.id).exists())
-
