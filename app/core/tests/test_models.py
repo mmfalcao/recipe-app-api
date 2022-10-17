3fba1776d6550946ca7faceef2d921eec1fd06a3
mmfalcao <marcelmf@msn.com>
2022-10-16 23:56:35 -0300

    add tags model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -72,3 +72,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
