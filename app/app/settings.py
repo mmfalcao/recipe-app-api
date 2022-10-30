600c8e2fcd0e333b726308b02b827f5613e5decb
mmfalcao <marcelmf@msn.com>
2022-10-29 23:46:56 -0300

    refactor settings to get environ

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,13 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&q$(d$)t7@za#4qbp@6btcbw_x9rd7$1j&0d7i@qz!_atn^ms7'
+# SECRET_KEY = 'django-insecure-&q$(d$)t7@za#4qbp@6btcbw_x9rd7$1j&0d7i@qz!_atn^ms7'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
